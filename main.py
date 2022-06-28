c8f483772c8a82322290020dcfda27d7a237b1b4
geon <gunho1107@naver.com>
2022-06-28 12:27:59 +0900

    add csv feature

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+import csv
+
 from request_html import request_html
 from parser import parse_html
 
 if __name__ == '__main__':
-    page = request_html(monthFrom=3, yearFrom=2022)
-    parse_html(page)
+    f = open('boxoffice.csv', 'w', newline='')
+    wr = csv.writer(f)
+
+    for year in range(2004, 2022):
+        for month in range(1, 13):
+            date = 'year {}, month {}'.format(year, month)
+            wr.writerow([date])
+            page = request_html(monthFrom=month, yearFrom=year)
+            data = parse_html(page)
+            wr.writerow(data)
+            print('month {}, year {} added'.format(month, year))
+
+    f.close()
--- parser.py
+++ parser.py
@@ -1,10 +1,19 @@
 from bs4 import BeautifulSoup as bs
+import csv
 
 
 def parse_html(html):
     soup = bs(html, "html.parser")
     rows = soup.find('tbody').find_all('tr')
 
+    data = []
     for row in rows:
-        print(row)
+        data_row = []
+        cols = row.find_all('td')
+        for col in cols:
+            data_row.append(col.text.strip())
+
+        data.append(data_row)
+
+    return data
 
